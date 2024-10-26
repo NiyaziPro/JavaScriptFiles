5b87fddd9782cfa2ff262edcedbe7cd8b29f70da
unknown <nyzjunior@gmail.com>
2024-10-26 10:52:38 +0200

    10:52 Saturday 26.10.2024

--- loops/script06.js
+++ loops/script06.js
@@ -1,8 +1,8 @@
-let row = 20;
+let row = 10;
 let star = "";
 
 for (let i = 0; i < row; i++) {
-  for (let k = 0; k <= row; k++) {
+  for (let k = 0; k <= row + i; k++) {
     if (i + k >= row) {
       star += "* ";
     } else {
---
+++ loops/script07.js
@@ -0,0 +1,12 @@
+let total = 0;
+let count = 100;
+
+
+while ((count <= 200) & (total <= 5000)) {
+  total += count;
+  count++;
+}
+console.log("Total: "+total);
+  console.log("Count: " + (count-100));
+
+ alert(Math.floor(Math.random()*100)+1) // random number creating between 1-100
